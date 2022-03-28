649ecc6983486527a1e015e884bd16cf8b65a298
Nina Spernaskaya <nina-sp@list.ru>
2022-03-28 21:13:08 +0700

    hw 3.4

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,6 +1,7 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 from students.models import Course
+from django.conf import settings
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -8,3 +9,11 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate_students(self, value):
+        if len(value) > settings.MAX_STUDENTS_PER_COURSE:
+            raise exceptions.ValidationError(
+                f"Максимальное число студентов на курсе: "
+                f"{settings.MAX_STUDENTS_PER_COURSE}"
+            )
+        return value
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,11 +1,9 @@
-# def test_example():
-#     assert False, "Just test example"
-
+import random
 import pytest
 from model_bakery import baker
 from rest_framework.test import APIClient
 
-from students.models import Course
+from students.models import Course, Student
 
 API_PATH = '/api/v1/courses/'
 
@@ -16,7 +14,15 @@ def client():
 
 
 @pytest.fixture
-def course_factory():
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.fixture
+def course_factory(student_factory):
     def factory(*args, **kwargs):
         return baker.make(Course, *args, **kwargs)
 
@@ -44,30 +50,34 @@ def test_get_courses(client, course_factory):
     assert response.status_code == 200
     data = response.json()
     assert len(courses) == len(data)
+    for i, c in enumerate(data):
+        assert courses[i].name == c['name']
 
 
 @pytest.mark.django_db
 def test_filter_course_id(client, course_factory):
-    courses = course_factory(_quantity=10)
+    courses = course_factory(_quantity=5)
+    random_course = random.choice(courses)
 
-    response = client.get(f'{API_PATH}?id={courses[0].id}')
+    response = client.get(f'{API_PATH}?id={random_course.id}')
 
     assert response.status_code == 200
     data = response.json()
     assert len(data) == 1
-    assert courses[0].name == data[0]['name']
+    assert random_course.name == data[0]['name']
 
 
 @pytest.mark.django_db
 def test_filter_course_name(client, course_factory):
-    courses = course_factory(_quantity=10)
+    courses = course_factory(_quantity=5)
+    random_course = random.choice(courses)
 
-    response = client.get(f'{API_PATH}?name={courses[0].name}')
+    response = client.get(f'{API_PATH}?name={random_course.name}')
 
     assert response.status_code == 200
     data = response.json()
     assert len(data) == 1
-    assert courses[0].id == data[0]['id']
+    assert random_course.id == data[0]['id']
 
 
 @pytest.mark.django_db
@@ -81,20 +91,44 @@ def test_create_course(client):
 
 
 @pytest.mark.django_db
-def test_update_course(client,course_factory):
+def test_update_course(client, course_factory, student_factory):
     course = course_factory()
-    data = {'name': 'course1'}
+    students = student_factory(_quantity=10)
+    student_ids = [student.id for student in students]
+    data = {'name': 'course1', 'students': student_ids}
 
     response = client.patch(f'{API_PATH}{course.id}/', data)
 
+    response_data = response.json()
     assert response.status_code == 200
-    assert response.json()['name'] == data['name']
+    assert response_data['name'] == data['name']
+    assert len(response_data['students']) == len(students)
 
 
 @pytest.mark.django_db
-def test_delete_course(client,course_factory):
+def test_delete_course(client, course_factory):
     course = course_factory()
 
     response = client.delete(f'{API_PATH}{course.id}/')
 
     assert response.status_code == 204
+
+
+@pytest.mark.parametrize(
+    'max_count, students_count, response_status',
+    [(2, 4, 400), (2, 1, 200)]
+)
+@pytest.mark.django_db
+def test_max_students(settings, client,
+                      course_factory, student_factory,
+                      max_count, students_count, response_status):
+    settings.MAX_STUDENTS_PER_COURSE = max_count
+    course = course_factory()
+    students = student_factory(_quantity=students_count)
+    student_ids = [student.id for student in students]
+
+    data = {'students': student_ids}
+
+    response = client.patch(f'{API_PATH}{course.id}/', data)
+
+    assert response.status_code == response_status
